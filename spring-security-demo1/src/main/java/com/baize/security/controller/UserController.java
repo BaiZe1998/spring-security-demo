37ec960ce0eea1093ee864b73739ab4123bb00ca
YLTFY1998 <1157467179@qq.com>
2021-05-05 14:30:29 +0800

    添加权限

--- spring-security-demo1/src/main/java/com/baize/security/controller/UserController.java
+++ spring-security-demo1/src/main/java/com/baize/security/controller/UserController.java
@@ -13,6 +13,10 @@ public class UserController {
 
     @RequestMapping("/user")
     public String user(Model model){
+        /**
+         * SecurityContextHolder.getContext().getAuthentication()获取到了穿过整个过滤器链的Authentication实例
+         * Authentication实例内记录了用户的各种信息，包括用户名，密码，权限等
+         */
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         String authority = SecurityContextHolder.getContext().getAuthentication().getAuthorities().toString();
         model.addAttribute("username", username);
