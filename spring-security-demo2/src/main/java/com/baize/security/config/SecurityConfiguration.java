c40487c1a6ab4019121d54f0d11ada8f813e030b
YLTFY1998 <1157467179@qq.com>
2021-05-05 17:39:36 +0800

    创建demo2

---
+++ spring-security-demo2/src/main/java/com/baize/security/config/SecurityConfiguration.java
@@ -0,0 +1,53 @@
+package com.baize.security.config;
+
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ *  Spring Security 配置类
+ */
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        /**
+         * 在内存中创建一个名为 "user" 的用户，密码为 "123"，拥有 "USER" 权限，密码使用BCryptPasswordEncoder加密
+         */
+        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("user").password(new BCryptPasswordEncoder().encode("123")).roles("USER");
+        /**
+         * 在内存中创建一个名为 "admin" 的用户，密码为 "123"，拥有 "USER" 和"ADMIN"权限
+         */
+        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("admin").password(new BCryptPasswordEncoder().encode("123")).roles("USER","ADMIN");
+    }
+
+    /**
+     * 给出了访问指定路径下的资源需要的角色（权限）
+     * /user/**表示访问/user/目录下的所有资源都需要USER权限
+     * /admin/**表示访问/admin/目录下的所有资源都需要ADMIN权限
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/","/index").permitAll()
+                .antMatchers("/user/**").hasRole("USER")
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .and()
+                .formLogin()
+                .loginPage("/login")    //用户未登录时，访问任何需要权限的资源都转跳到该路径，即登录页面，此时登陆成功后会继续跳转到第一次访问的资源页面（相当于被过滤了一下）
+                .defaultSuccessUrl("/user") //登录认证成功后默认转跳的路径，意思时admin登录后也跳转到/user
+                .and()
+                .logout()
+                .logoutUrl("/logout")   //退出登陆的路径，指定spring security拦截的注销url,退出功能是security提供的
+                .logoutSuccessUrl("/login");//用户退出后要被重定向的url
+    }
+}
---
+++ spring-security-demo2/src/main/java/com/baize/security/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.baize.security.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("/user")
+    public String user(Model model){
+        /**
+         * SecurityContextHolder.getContext().getAuthentication()获取到了穿过整个过滤器链的Authentication实例
+         * Authentication实例内记录了用户的各种信息，包括用户名，密码，权限等
+         */
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        String authority = SecurityContextHolder.getContext().getAuthentication().getAuthorities().toString();
+        model.addAttribute("username", username);
+        model.addAttribute("authority", authority);
+        return "user/user";
+    }
+    @RequestMapping("/admin")
+    public String admin(Model model){
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        String authority = SecurityContextHolder.getContext().getAuthentication().getAuthorities().toString();
+        model.addAttribute("username", username);
+        model.addAttribute("authority", authority);
+        return "admin/admin";
+    }
+}
